bb5262982aa64e274126d4a6b18ea26993edeb65
Tua <hoangtua.vn@gmail.com>
2020-09-13 02:00:47 +0000

    install vim plugins

---
+++ vps/install-vim.sh
@@ -0,0 +1,7 @@
+# Install NERDTree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+
+# Install Line Indents
+git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/vendor/start/indentLine
+vim -u NONE -c "helptags  ~/.vim/pack/vendor/start/indentLine/doc" -c "q"
